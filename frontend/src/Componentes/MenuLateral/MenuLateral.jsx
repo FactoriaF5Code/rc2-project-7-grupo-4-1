eeea2ed580e6cb50811355d8ce7385524f1116b9
Marta <martalarena2002@gmail.com>
2024-01-26 10:15:49 +0100

    menu escondido

--- frontend/src/Componentes/MenuLateral/MenuLateral.jsx
+++ frontend/src/Componentes/MenuLateral/MenuLateral.jsx
@@ -1,27 +1,34 @@
 import "./MenuLateral.css";
 import login from "../../assets/login.png";
 import lupa from "../../assets/lupa.png";
-import arrow from "../../assets/Arrow 1.png"
+import arrow from "../../assets/Arrow 1.png";
+import { useState } from "react";
 
 export default function MenuLateral() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const mostrarse = () => setShowMenu(!showMenu);
   return (
     <section className="contenedor">
-    <div className="menuLateral">
-      <div className="login">
-        <img className="login__img" src={login} alt="login" />
-        <p className="login__description">Login</p>
+      <div className={`menuLateral ${showMenu ? `mostrar` : ""}`}>
+        <div className="login">
+          <img className="login__img" src={login} alt="login" />
+          <p className="login__description">Login</p>
+        </div>
+        <div className="search">
+          <input
+            className="search__input"
+            type="text"
+            placeholder="¿Que estas buscando?"
+          />
+          <img className="search__img" src={lupa} alt="lupa" />
+        </div>
+        <button className="menuLateral__button">Añadir</button>
       </div>
-      <div className="search">
-        <input
-          className="search__input"
-          type="text"
-          placeholder="¿Que estas buscando?"
-        />
-        <img className="search__img" src={lupa} alt="lupa" />
-      </div>
-      <button className="menuLateral__button">Añadir</button>
-    </div>
-    <button className="button__esconderse"><img className="button__esconderse__arrow" src={arrow} alt="flecha" /></button>
+      {showMenu}
+      <button className="button__mostrarse" onClick={mostrarse}>
+        <img className="button__mostrarse__arrow" src={arrow} alt="flecha" />
+      </button>
     </section>
   );
 }
