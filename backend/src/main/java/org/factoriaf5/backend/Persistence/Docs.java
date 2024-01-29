9bff0df95542837f5d3b10dd2163d5b5099b7809
evavelasco <evapibiyal@gmail.com>
2024-01-29 13:51:26 +0100

    empezando repositorio

--- backend/src/main/java/org/factoriaf5/backend/Persistence/Docs.java
+++ backend/src/main/java/org/factoriaf5/backend/Persistence/Docs.java
@@ -1,5 +1,46 @@
 package org.factoriaf5.backend.Persistence;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "docs")
 public class Docs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String title;
+    String url;
+    String comments;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public Docs() {
+
+    }
+
+    public Docs(Long id, String title, String url, String comments) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+        this.comments = comments;
+    }
 
 }
