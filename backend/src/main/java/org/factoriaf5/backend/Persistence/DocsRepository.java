41cbcdac5700ec5db07ac2a6bdd3452b063538fe
evavelasco <evapibiyal@gmail.com>
2024-02-05 15:31:52 +0100

    delete en controller

--- backend/src/main/java/org/factoriaf5/backend/Persistence/DocsRepository.java
+++ backend/src/main/java/org/factoriaf5/backend/Persistence/DocsRepository.java
@@ -2,7 +2,9 @@ package org.factoriaf5.backend.Persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface DocsRepository extends JpaRepository<Docs, Long> {
+
 }
--- backend/src/main/java/org/factoriaf5/backend/controllers/DocsController.java
+++ backend/src/main/java/org/factoriaf5/backend/controllers/DocsController.java
@@ -6,8 +6,11 @@ import java.util.List;
 import org.factoriaf5.backend.Persistence.Docs;
 import org.factoriaf5.backend.Persistence.DocsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +42,15 @@ public class DocsController {
 
     public DocsController(@Autowired DocsRepository repository) {
         this.repository = repository;
+    }
+
+    @DeleteMapping("/docs/{id}")
+    public ResponseEntity<DocsResponse> deleteDocs(@PathVariable Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
